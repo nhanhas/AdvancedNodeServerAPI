185adace1c15d628e85df89572d41757fb8e4f2f
MPereira <mpereira132@hotmail.com>
2016-04-10 01:06:16 +0100

    #S02
    bug fix

--- app/server.js
+++ app/server.js
@@ -9,10 +9,6 @@ var bodyParser = require('body-parser');    // define our app using body-parser
 var mongoose   = require('mongoose');       // define our app using mongoose
 mongoose.connect('mongodb://localhost:27017/test'); // connect to our database
 
-//All API Routes goes Here...
-var servicesConfig = require('./BusinessLayer/ServicesConfig');
-app.use(servicesConfig);
-
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -20,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
+
+
+//All API Routes goes Here...
+var servicesConfig = require('./BusinessLayer/ServicesConfig');
+app.use(servicesConfig);
 
 
 // START THE SERVER
